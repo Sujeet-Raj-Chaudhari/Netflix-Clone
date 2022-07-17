e4ec9b26f9f0933a3a82f4ec6d2c450d2d22eda0
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2022-07-17 19:04:20 +0530

    Backend Corrections

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const authRoute = require("./routes/auth");
+
 dotenv.config();
 
 mongoose
@@ -13,6 +15,9 @@ mongoose
   })
   .then(() => console.log("DB Connection Successful"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 
-const User = require("../model/User");
+const User = require("../models/User");
 
 //REGISTER
-router.post("/register", (req.res) =>{
-    const newUser = new User({
-        username: req.body.username
-    })
-}) 
+router.post("/register", async (req, res) => {
+  const newUser = User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const user = await newUser.save();
+
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
