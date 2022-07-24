ec5f8937f6ba0fc6b25b70e261ee129165b91aef
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2022-07-24 17:59:23 +0530

    Till Users Routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
 
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
   const newUser = User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
   });
 
   try {
@@ -16,6 +22,37 @@ router.post("/register", async (req, res) => {
     res.status(201).json(user);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json("Wrong Password or Username!");
+    }
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    if (originalPassword !== req.body.password)
+      return res.status(401).json("Wrong Password or Username");
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+
+    //Hiding Password from console of Thunderclient
+    const { password, ...info } = user._doc;
+
+    return res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    return res.status(500).json(err);
   }
 });
 module.exports = router;
